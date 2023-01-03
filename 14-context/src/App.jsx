acf8dffae1dd76b30cf2d538489cd6bfc92138b5
Rosen Toshev <rtoscheff@gmail.com>
2023-01-04 00:08:41 +0200

    Added code splitting (lazy and Suspense), server-side rendering with pipeable streams serving the non-interactive part of the code first and express added to the project to serve the stream

--- 14-context/src/App.jsx
+++ 14-context/src/App.jsx
@@ -1,10 +1,7 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 import AdoptedPetContext from "./AdoptedPetContext";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,27 +12,41 @@ const queryClient = new QueryClient({
   },
 });
 
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
+
 const App = () => {
   const adoptedPet = useState(null);
   return (
-    <div>
-      <BrowserRouter>
-        <AdoptedPetContext.Provider value={adoptedPet}>
-          <QueryClientProvider client={queryClient}>
-            <header>
-              <Link to="/">Adopt Me!</Link>
+    <div
+      className="m-1 p-0"
+      style={{
+        background: "url(https://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
+      }}
+    >
+      <AdoptedPetContext.Provider value={adoptedPet}>
+        <QueryClientProvider client={queryClient}>
+          <Suspense
+            fallback={
+              <div className="loading-pane">
+                <h2 className="loader">🌀</h2>
+              </div>
+            }
+          >
+            <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
+              <Link className="text-6xl text-white hover:text-gray-200" to="/">
+                Adopt Me!
+              </Link>
             </header>
             <Routes>
               <Route path="/details/:id" element={<Details />} />
               <Route path="/" element={<SearchParams />} />
             </Routes>
-          </QueryClientProvider>
-        </AdoptedPetContext.Provider>
-      </BrowserRouter>
+          </Suspense>
+        </QueryClientProvider>
+      </AdoptedPetContext.Provider>
     </div>
   );
 };
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+export default App;
