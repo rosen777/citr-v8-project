ebb50780efd553051a1ae96739909e9942a5eabe
Rosen Toshev <rtoscheff@gmail.com>
2023-01-29 16:41:56 +0000

    Added the hook and snapshot tests

--- 14-context/src/Results.jsx
+++ 14-context/src/Results.jsx
@@ -4,7 +4,7 @@ const Results = ({ pets }) => {
   return (
     <div className="search">
       {!pets.length ? (
-        <h1>No Pets Found</h1>
+        <h1>No Pets Found!</h1>
       ) : (
         pets.map((pet) => {
           return (
